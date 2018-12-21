ae642b16d3000c6d8568b7a43d0f4798c34499b0
Ignacio Nelson <info@subwaydesign.com.ar>
2018-12-21 01:07:44 -0300

    Reproduced the commit from the main repo by trini to add more file extensions
    https://github.com/projectsend/projectsend/pull/649

--- install/database.php
+++ install/database.php
@@ -243,7 +243,7 @@ if (defined('TRY_INSTALL')) {
 								('logo_thumbnails_folder', '/img/custom/thumbs'),
 								('timezone', 'America/Argentina/Buenos_Aires'),
 								('timeformat', 'd/m/Y'),
-								('allowed_file_types', '7z,ace,ai,avi,bin,bmp,cdr,doc,docm,docx,eps,fla,flv,gif,gz,gzip,htm,html,iso,jpeg,jpg,mp3,mp4,mpg,odt,oog,ppt,pptx,pptm,pps,ppsx,pdf,png,psd,rar,rtf,tar,tif,tiff,txt,wav,xls,xlsm,xlsx,zip'),
+								('allowed_file_types', '7z,ace,ai,avi,bin,bmp,bz2,cdr,doc,docm,docx,eps,fla,flv,gif,gz,gzip,htm,html,iso,jpeg,jpg,mp3,mp4,mpg,odt,oog,ppt,pptx,pptm,pps,ppsx,pdf,png,psd,rar,rtf,tar,tif,tiff,tgz,txt,wav,xls,xlsm,xlsx,xz,zip'),
 								('logo_filename', 'logo.png'),
 								('admin_email_address', :email),
 								('clients_can_register', '0'),
